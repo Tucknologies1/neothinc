1516d34e308736c73daa83fadb0393bd82d09c13
Van Tran <kudoszvan@gmail.com>
2017-11-11 14:54:05 -0500

    Added component specific routes

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AboutUs extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the about us</h1>
+            </div>
+        );
+    }
+}
+
+export default AboutUs
---
+++ src/Pages/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Loading extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the loading page</h1>
+            </div>
+        );
+    }
+}
+
+export default Loading
