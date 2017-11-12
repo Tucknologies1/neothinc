f746c68375678e11fce91047767a1d9751e01f6c
Van Tran <kudoszvan@gmail.com>
2017-11-11 21:15:16 -0500

    Added Google fonts cairo and abel

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 class Home extends React.Component {
     render() {
         return(
-            <div>
-                <h1>This is the home component</h1>
-                
+            <div className="homeContainer">
+                <h1>THiNC TECHNOLOGY</h1>
+                <h1>WEB MOBILE SOFTWARE</h1>
+                <button className="getStartedButton">Get Started</button>
             </div>
         );
     }
