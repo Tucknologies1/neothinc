6115822ae85e336b1e812f4881b650c9c61d9d8e
Matthew Yates <yatesm4@mail.lcc.edu>
2017-11-11 23:11:27 -0500

    Animated Icon

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Header from './Components/Header';
 
 class Home extends React.Component {
     render() {
         return(
             <div>
+                <Header />
                 <h1>This is the home component</h1>
             </div>
         );
