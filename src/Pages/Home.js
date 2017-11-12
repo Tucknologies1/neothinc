914aa2c7a459e3a7a7e15fd6e785fa0ae04a70d4
Van Tran <kudoszvan@gmail.com>
2017-11-11 21:42:34 -0500

    Added semantic ui to be able to be used styled home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-
+import { Container, Button } from 'semantic-ui-react';
 class Home extends React.Component {
     render() {
         return(
             <div className="homeContainer">
+                <Container textAlign='center'>
                 <h1>THiNC TECHNOLOGY</h1>
-                <h1>WEB MOBILE SOFTWARE</h1>
-                <button className="getStartedButton">Get Started</button>
+                <h2>WEB MOBILE SOFTWARE</h2>
+                <Button>
+                    Get Started
+                </Button>
+                </Container>
             </div>
         );
     }
