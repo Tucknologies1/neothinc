ed71824f7270ad72571d88a12b08a919a5d2ac66
Van Tran <kudoszvan@gmail.com>
2017-11-22 14:33:22 -0500

    Finished Q&A page

--- src/Pages/Components/QuestionAndAnswer.js
+++ src/Pages/Components/QuestionAndAnswer.js
@@ -1,16 +1,35 @@
 import React, {Component} from "react";
-import { Container } from 'semantic-ui-react';
+import { Container, Reveal} from 'semantic-ui-react';
+
 
 class QuestionAndAnswer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render(){
         return(
-            <div className="questionAndAnswer">
-                <div className="qaCard">
-                    Q: 
-                    <br/>
-                    A:
-                </div>
+            <div className="revealContainer">
+                <Reveal animated='move'>
+                    <Reveal.Content visible>
+                        <div className="questionVisible qaCard">
+                            <p>{this.props.question}</p>
+                        </div>
+                    </Reveal.Content>
+                    <Reveal.Content hidden>
+                        <div className="answerHidden">
+                            <p>{this.props.answer}</p>
+                        </div>
+                    </Reveal.Content>
+                </Reveal>
             </div>
+            // <div className="questionAndAnswer">
+            //     <div className="qaCard">
+            //         Q:
+            //         <br/>
+            //         A:
+            //     </div>
+            // </div>
         );
     }
 }
